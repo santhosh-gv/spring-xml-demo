506cd3c32a260c6863579255c82a5a77efd57e4a
santhosh-gv <santhosh2596@gmail.com>
2019-10-03 17:42:14 +0530

    task 6 commit

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,6 +1,7 @@
 package com.stackroute;
 
 import com.stackroute.demo.BeanLifeCycleDemoBean;
+import com.stackroute.demo.BeanPostProcessorDemoBean;
 import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,13 +12,10 @@ public class Main {
         //Task 5
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        BeanLifeCycleDemoBean movie2 = applicationContext.getBean("beanLifecycle",BeanLifeCycleDemoBean.class);
+        BeanPostProcessorDemoBean movie2 = applicationContext.getBean("beanPostProcessor", BeanPostProcessorDemoBean.class);
         //System.out.println("Removed Autowire byType and added constructor injection");
-        movie2.afterPropertiesSet();
-        movie2.destroy();
+        movie2.postProcessBeforeInitialization("beanPostProcessor","postProcessor");
+        movie2.postProcessAfterInitialization("beanPostProcessor","postProcessor");
         
-
-
-
     }
 }
